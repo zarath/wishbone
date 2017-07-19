52c05c61c847a1c94f73b20adb052437e59d6012
Jelle Smet <wishbone@smetj.net>
2017-07-19 22:04:08 +0200

    one object instance is one file

--- wishbone/utils/structured_data_file.py
+++ wishbone/utils/structured_data_file.py
@@ -40,49 +40,54 @@ class StructuredDataFile():
     the provided schema.  The datastructure can be JSON and/or YAML.
 
     Attributes:
+        default (obj): The default value when no file content has loaded yet.
         schema (str): The JSONschema to validate the loaded data against.
         expect_json (bool): When True, the data structure can be JSON
         expect_yaml (bool): When True, the data structure can be YAML.
         content (dict): The loaded configurations.  Absolute paths are dict keys.
     '''
 
-    def __init__(self, schema=None, expect_json=True, expect_yaml=True):
+    def __init__(self, default=None, schema=None, expect_json=True, expect_yaml=True):
         '''
         Args:
+            default (obj): A default value to return when no file content has loaded yet.
             schema (str): The JSONschema to validate the loaded data against.
             expect_json (bool): When True, the data structure can be JSON
             expect_yaml (bool): When True, the data structure can be YAML.
         '''
 
+        self.default = default
         self.schema = schema
         self.expect_json = expect_json
         self.expect_yaml = expect_yaml
-        self.content = {}
+        self.content = None
         self.lock = Semaphore()
+        self.current_filename = None
 
     def delete(self, path):
         '''Deletes the file content from the object'''
 
         with self.lock:
-            try:
-                del(self.content[path])
-            except Exception:
-                pass
+            self.content = None
 
     def dump(self):
         '''Dumps the complete content'''
 
         with self.lock:
-            return self.content
+            if self.content is None:
+                return self.default
+            else:
+                return self.content
 
     def get(self, path):
-        '''Returns the content of the file.  If the file isn't loaded yet, it
+        '''Returns the cached content of the file.  If the file isn't loaded yet, it
         tries to do that.'''
 
         with self.lock:
-            if path not in self.content:
-                self.__load(path)
-            return self.content[path]
+            if self.content is None or os.path.abspath(path) != self.current_filename:
+                return self.__load(path)
+            else:
+                return self.content
 
     def load(self, path):
         '''Loads the file into the module and validates the content when required.'''
@@ -97,15 +102,18 @@ class StructuredDataFile():
                 self.__readFile(path)
                 if self.schema is not None:
                     try:
-                        validate(self.content[path], self.schema)
+                        validate(self.content, self.schema)
                     except Exception as err:
-                        raise InvalidData(err.message.replace("\n", " -> "))
+                        # error = str(err)
+                        # error = error.replace("\n", " ->")
+                        # raise InvalidData(" ".join(str(error).split()))
+                        raise InvalidData(err.message)
             else:
                 raise Exception("'%s' does not appear to be a regular file.")
         else:
             raise Exception("File '%s' does not exist or is not accessible." % (path))
 
-        return self.content[path]
+        return self.content
 
     def __readFile(self, path):
 
@@ -134,11 +142,11 @@ class StructuredDataFile():
     def __readJSON(self, f):
         f.seek(0)
         data = json.load(f)
-        self.content[f.name] = data
+        self.content = data
         return True
 
     def __readYAML(self, f):
         f.seek(0)
         data = yaml.load(f)
-        self.content[f.name] = data
+        self.content = data
         return True
